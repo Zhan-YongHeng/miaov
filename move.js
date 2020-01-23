644dd07fa16d36b0f63edaef2b86f563532f2935
Zhan-YongHeng <2390778826@qq.com>
2020-01-23 20:22:34 +0800

    text2 and text

--- move.js
+++ move.js
@@ -1,4 +1,4 @@
 function drag(id){
-    /* var obj= document.querySelectorAll(id0);
-    obj.onmousedown=function(){}; */
+    var obj= document.querySelectorAll(id0);
+    obj.onmousedown=function(){};
 }
